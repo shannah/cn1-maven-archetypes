d47724bb49a826de9c97972ae09e8406b3e83f94
Steve Hannah <steve@weblite.ca>
2021-03-24 05:48:10 -0700

    Updated deploy and update versions scripts with some comments.

--- deploy-to-sonatype.sh
+++ deploy-to-sonatype.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
+# deploy-to-sonatype.sh
+# Created by Steve Hannah, March 24, 2021
+#
+# Synopsis:
+# --------
+#
+# Deploys project to Maven central.  You should run update-version.sh before and after
+# running this to set the release version, and then create a new SNAPSHOT version.
+#
+# **IMPORTANT**
+# Before running this, you should deploy the main Codename One project to maven central.
+# After running this, you still need to log into the Sonatype console to close, and release
+# the staging repository.
+#
+# Typical Workflow is:
+# bash update-version 8.0.1 && bash deploy-to-sonatype.sh && bash update-version 8.0.2-SNAPSHOT
+set -e
 export GPG_TTY=$(tty)
 mvn deploy -Psign-artifacts
--- update-version.sh
+++ update-version.sh
@@ -1,4 +1,36 @@
 #!/bin/bash
+# update-version.sh
+# Created by Steve Hannah.  March 24, 2021
+#
+# Synopsis:
+# ---------
+#
+# Updates the version of this project and commits the version changes in Git.  If the version
+# is a release version (i.e. not ending in '-SNAPSHOT', then it will also add a git tag).
+#
+# Typically you would run this before and after running the deploy-to-sonatype.sh script.  Before
+# to set the release version, and after to change to a new SNAPSHOT version.
+#
+# **IMPORTANT**
+# Before running this, you must update the version in the Main Codename One repo.  If it can't
+# find a version of Codename One in the local maven repo to match the version that you're updating
+# to, it will fail.
+#
+# Usage:
+#  bash update-version.sh VERSION
+#
+# Arguments:
+#  VERSION    The version number to update to.
+#
+# Examples:
+#  bash update-version.sh 8.0.1
+#     Updates to version 8.0.1
+#
+#  bash update-version.sh 8.0.2-SNAPSHOT
+#     Updates to version 8.0.2-SNAPSHOT
+#
+# Typical Workflow is:
+# bash update-version.sh 8.0.1 && bash deploy-to-sonatype.sh && bash update-version.sh 8.0.2-SNAPSHOT
 set -e
 if [ -z $1 ]; then
   echo "Usage bash update-version.sh VERSION"
@@ -25,6 +57,15 @@ f=./cn1app-archetype/src/test/resources/projects/basic/archetype.properties
 perl -pi -e "s/^cn1Version=$oldVersion\$/cn1Version=$version/g" "$f"
 f=./cn1lib-archetype/src/test/resources/projects/basic/archetype.properties
 perl -pi -e "s/^cn1Version=$oldVersion\$/cn1Version=$version/g" "$f"
+
+echo "Committing version change in git"
+git commit -a -m "Updated version to $version"
+if [[ "$version" == *-SNAPSHOT ]]; then
+  echo "This is a snapshot version so not adding a tag"
+else
+  echo "Adding git tag for 'v${version}'"
+  git tag -a "v${version}" -m "Version ${version}"
+fi
 
 
 
